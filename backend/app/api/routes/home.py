4bc00195300e4c3a56aa00ee71acea078e401702
Arsh Singhal <singhalarsh2001@gmail.com>
2025-12-05 23:01:05 +0530

    Added logging

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from app.core.logging import get_logger
+
+logger = get_logger()
 
 router = APIRouter(
     prefix="/home",
@@ -7,4 +10,9 @@ router = APIRouter(
 
 @router.get("/")
 def home():
+    logger.info("Home page accessed !")
+    logger.debug("Home page accessed !")
+    logger.error("Home page accessed !")
+    logger.warning("Home page accessed !")
+    logger.critical("Home page accessed !")
     return {"message": "Welcome to the Bank API"}
---
+++ backend/app/core/logging.py
@@ -0,0 +1,38 @@
+import os
+from loguru import logger
+from app.core.config import settings
+
+logger.remove()
+
+LOG_DIR = os.path.join(os.path.dirname(__file__), "logs")   #backend/app/logs
+
+LOG_FORMAT = (
+    "{time:YYYY-MM-DD HH:mm:ss.SSSS} | "
+    "{level: <8} | "
+    "{name}:{function}:{line} - "
+    "{message}"
+)
+
+logger.add(
+    sink=os.path.join(LOG_DIR, "debug.log"),
+    format=LOG_FORMAT,
+    level="DEBUG" if settings.ENVIRONMENT == "local" else "INFO",
+    filter= lambda record: record["level"].no <= logger.level("WARNING").no,
+    rotation="10MB",
+    compression="zip",
+    retention="30 days",
+)
+
+logger.add(
+    sink=os.path.join(LOG_DIR, "error.log"),
+    format=LOG_FORMAT,
+    level="ERROR",
+    rotation="10MB",
+    compression="zip",
+    retention="30 days",
+    backtrace=True,
+    diagnose=True,
+)
+
+def get_logger():
+    return logger
